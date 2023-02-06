6d4560289577bd39becf82a981c193002e94f573
Gianni <gianniicherry@gmail.com>
2023-02-06 13:13:42 -0800

    commit tree 6848c8423b12b2110d6e79cce215a29be9425ee5

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"  
+end
+greet("Naureen")
+greet("Jimmy")
+
+def greet_with_default(name ="programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default("Sunny")
+
+def add(num1, num2) 
+    num1 + num2
+end
+add(1,2)
+
+def halve(num)
+    if num.class == String
+        return nil
+    end
+    num / 2
+end
+
